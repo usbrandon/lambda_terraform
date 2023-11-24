6b5d534631d56b805d8bcb2fedec67ab9273371e
Brandon Jackson <usbrandon@gmail.com>
2023-11-24 01:28:36 -0600

    Use a docker container and ECR as the layer for Lambda. With Pandas, Numpy and Pyarrow, there was no way to get under 300MB.
    aws ecr create-repository --repository-name my-lambda-function --region us-east-1
    
    docker build -t my-lambda-function .
    
    aws ecr get-login-password --region us-east-1 --profile acloudguru | docker login --username AWS --password-stdin 748487089563.dkr.ecr.us-east-1.amazonaws.com
    
    docker tag my-lambda-function:latest 748487089563.dkr.ecr.us-east-1.amazonaws.com/my-lambda-function:latest
    
    docker push 748487089563.dkr.ecr.us-east-1.amazonaws.com/my-lambda-function:latest

--- lambda.tf
+++ lambda.tf
@@ -1,15 +1,9 @@
-data "archive_file" "python_lambda_package" {  
-  type = "zip"  
-  source_file = "${path.module}/code/lambda_function.py" 
-  output_path = "lambda_function.zip"
-}
-
+# AWS Lambda function resource
 resource "aws_lambda_function" "test_lambda_function" {
-        function_name = "lambdaTest"
-        filename      = "lambda_function.zip"
-        source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
-        role          = aws_iam_role.lambda_role.arn
-        runtime       = "python3.11"
-        handler       = "lambda_function.lambda_handler"
-        timeout       = 10
+  function_name = "lambdaTest"
+  package_type  = "Image"
+  image_uri     = "748487089563.dkr.ecr.us-east-1.amazonaws.com/my-lambda-function:latest"
+  role          = aws_iam_role.lambda_role.arn
+  timeout       = 10
+  
 }
